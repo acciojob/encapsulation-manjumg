dde2e1bcfea90b1a760eec9e2a3be6f2dd471f20
manjumg <manjugoni007@gmail.com>
2024-08-08 15:39:31 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        
+        // Using setter to set the name
+        obj.setName("John");
+
+        // Using getter to access the name
+        System.out.println(obj.getName());  // Output will be "John"
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Getter function
+    public String getName() {
+        return name;
+    }
+
+    // Setter function
+    public void setName(String name) {
+        this.name = name;
+    }
 }
